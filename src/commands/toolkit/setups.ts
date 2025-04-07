0fd8887c4e83dc56a8b6831b4ac1eaf1dc09a414
ZakaHaceCosas <zakahacecosas@protonmail.com>
2025-04-07 22:16:56 +0200

    make setups standalone files for easier maintenance

--- src/commands/toolkit/setups.ts
+++ src/commands/toolkit/setups.ts
@@ -1,169 +1,62 @@
 import { ColorString } from "../../functions/io.ts";
-import type { FkNodeYaml } from "../../types/config_files.ts";
 
-const commons = {
-    buildTargets: ["dist/", "out/", "build/", ".cache/"],
-    baseJsGitignore: [
-        "# built / output files",
-        ...["dist/", "out/", "build/", ".cache/"],
-        ".deno/",
-        "",
-        "# environment",
-        ".env",
-        ".env.local",
-        ".env.*.local",
-        "",
-        "# logs",
-        "npm-debug.log*",
-        "yarn-debug.log*",
-        "yarn-error.log*",
-        "bun-debug.log*",
-        "lerna-debug.log*",
-        "",
-        "# caches",
-        ".npm/",
-        ".bun/",
-        ".pnp/",
-        ".pnp.js",
-        "deno_dir/",
-        "",
-        "# modules",
-        "node_modules/",
-    ].join("\n"),
-    jsLockfiles: ["deno.lock", "bun.lock", "bun.lockb", "yarn.lock", "pnpm-lock.yaml", "package-lock.json"],
-};
-
-const ACTUAL_SETUPS: {
-    fknodeBasic: FkNodeYaml;
-    fknodeAllowAll: FkNodeYaml;
-    gitignoreJs: string;
-    gitignoreJsNoLock: string;
-    tsStrictest: object;
-    tsLibrary: object;
-    editorConfigDefault: string;
-} = {
-    fknodeBasic: {
-        destroy: {
-            intensities: ["hard"],
-            targets: commons.buildTargets,
-        },
-    },
-    fknodeAllowAll: {
-        divineProtection: "disabled",
-        destroy: {
-            intensities: ["*"],
-            targets: commons.buildTargets,
-        },
-        flagless: {
-            flaglessCommit: false,
-            flaglessDestroy: true,
-            flaglessLint: true,
-            flaglessPretty: true,
-            flaglessUpdate: true,
-        },
-        commitActions: true,
-    },
-    gitignoreJs: commons.baseJsGitignore,
-    gitignoreJsNoLock: `${commons.baseJsGitignore}\n${commons.jsLockfiles}`,
-    tsStrictest: {
-        compilerOptions: {
-            "strict": true,
-            "strictNullChecks": true,
-            "strictBindCallApply": true,
-            "strictFunctionTypes": true,
-            "strictBuiltinIteratorReturn": true,
-            "strictPropertyInitialization": true,
-            "noImplicitAny": true,
-            "noImplicitOverride": true,
-            "noUnusedLocals": true,
-            "noUnusedParameters": true,
-            "noPropertyAccessFromIndexSignature": true,
-            "noUncheckedIndexedAccess": true,
-            "noUncheckedSideEffectImports": true,
-            "noImplicitThis": true,
-            "noImplicitReturns": true,
-            "noFallthroughCasesInSwitch": true,
-            "forceConsistentCasingInFileNames": true,
-            "exactOptionalPropertyTypes": true,
-            "useUnknownInCatchVariables": true,
-        },
-    },
-    tsLibrary: {
-        "compilerOptions": {
-            "target": "ES2022",
-            "module": "ESNext",
-            "declaration": true,
-            "outDir": "dist",
-            "strict": true,
-            "moduleResolution": "nodenext",
-            "esModuleInterop": true,
-            "skipLibCheck": true,
-        },
-        "include": ["src"],
-    },
-    editorConfigDefault: [
-        "root = true",
-        "",
-        "[*]",
-        "indent_style = space",
-        "indent_size = 4",
-        "end_of_line = lf",
-        " charset = utf-8 ",
-        "trim_trailing_whitespace = true",
-        " insert_final_newline = true",
-        "",
-
-        "[*.md]",
-        "trim_trailing_whitespace = false",
-    ].join("\n"),
-};
+/** Get an embed file. */
+function Get(name: string): string {
+    const dir = Deno.readDirSync(import.meta.dirname + "/setups");
+    let result: string | null = null;
+    for (const match of dir) {
+        if (match.isFile && match.name === name) result = Deno.readTextFileSync(import.meta.dirname + "/setups/" + match.name);
+    }
+    if (!result) throw new Error("Invalid name: " + name);
+    return result;
+}
 
 export const SETUPS: {
     name: string;
     desc: string;
-    content: string | FkNodeYaml;
+    content: string;
     seek: "fknode.yaml" | ".gitignore" | "tsconfig.json" | ".editorconfig";
 }[] = [
     {
         name: "fknode-basic",
         desc: "A very basic fknode.yaml file.",
-        content: ACTUAL_SETUPS.fknodeBasic,
+        content: Get("fknode-basic.yaml"),
         seek: "fknode.yaml",
     },
     {
         name: "fknode-allow-all",
         desc: "An fknode.yaml file that allows every feature to run (commits too!).",
-        content: ACTUAL_SETUPS.fknodeAllowAll,
+        content: Get("fknode-allow-all.yaml"),
         seek: "fknode.yaml",
     },
     {
         name: "gitignore-js",
         desc: "A gitignore file for JavaScript projects.",
-        content: ACTUAL_SETUPS.gitignoreJs,
+        content: Get(".gitignore-js"),
         seek: ".gitignore",
     },
     {
         name: "gitignore-js-nolock",
         desc: "A gitignore file for JavaScript projects (also ignores lockfiles).",
-        content: ACTUAL_SETUPS.gitignoreJsNoLock,
+        content: Get(".gitignore-js-nolock"),
         seek: ".gitignore",
     },
     {
         name: "ts-strictest",
         desc: "Strictest way of TypeScripting, ensuring cleanest code.",
-        content: ACTUAL_SETUPS.tsStrictest,
+        content: Get("ts-strictest.json"),
         seek: "tsconfig.json",
     },
     {
         name: "ts-library",
         desc: "Recommended config for libraries.",
-        content: ACTUAL_SETUPS.tsLibrary,
+        content: Get("ts-library.json"),
         seek: "tsconfig.json",
     },
     {
         name: "editorconfig-default",
         desc: "A basic .editorconfig file that works for everyone.",
-        content: ACTUAL_SETUPS.editorConfigDefault,
+        content: Get(".editorconfig-default"),
         seek: ".editorconfig",
     },
 ];
