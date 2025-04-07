b67d3f0a147bf1db26886a5efa8b194fea8b6aa8
ZakaHaceCosas <zakahacecosas@protonmail.com>
2025-04-07 19:22:37 +0200

    extract RCS to separate function

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -1,9 +1,10 @@
-import { StringUtils, type UnknownString } from "@zakahacecosas/string-utils";
+import { type UnknownString } from "@zakahacecosas/string-utils";
 import { I_LIKE_JS } from "../constants.ts";
 import { ColorString, LogStuff } from "../functions/io.ts";
 import { GetProjectEnvironment, SpotProject } from "../functions/projects.ts";
 import { NameProject } from "../functions/projects.ts";
 import type { FnCPF } from "../types/platform.ts";
+import { RecommendedCommunityStandards } from "./toolkit/rcs.ts";
 
 function StringifyDependencyRelationship(rel: FnCPF["deps"][0]["rel"]): string {
     return rel === "univ:dep"
@@ -57,65 +58,5 @@ export default function TheStatistics(target: UnknownString) {
 
     LogStuff("This is how your project compares to the Recommended Community Standards.", "chart");
 
-    // deno-lint-ignore no-explicit-any
-    const content = env.main.stdContent as any;
-
-    if (StringUtils.validateAgainst(env.runtime, ["node", "bun"])) {
-        const { private: p, license: l, author: a, contributors: c, description: d, repository: r, bugs: b, type: t } = content;
-
-        LogStuff(
-            p ? "This is a private project. Running generic checks." : "This is a public project. Running additional package checks.",
-            "bulb",
-        );
-
-        LogStuff(l ? `${l} license, good.` : "No license found. You should specify it!", l ? "tick" : "error");
-        if (!p || a) {
-            LogStuff(
-                a ? `Made by ${typeof a === "string" ? a : a.name}, nice.` : "No author found. Who made this 'great' code?",
-                a ? "tick" : "error",
-            );
-        }
-        LogStuff(
-            c
-                ? `So we've got ${c.length} contributor(s), great.`
-                : "No contributors found. If none, that's fine, but if someone helps, mention them.",
-            c ? "tick" : "bruh",
-        );
-        LogStuff(
-            d ? `${ColorString(d, "italic")}, neat description.` : "No description found. What does your code even do?",
-            d ? "tick" : "error",
-        );
-        if (!p) {
-            LogStuff(r ? `Known repo URL, awesome.` : "You didn't specify a repository, why?", r ? "tick" : "error");
-            LogStuff(
-                b ? `Known bug-report URL, incredible.` : "You didn't specify where to report bugs. Don't be lazy and accept feedback!",
-                b ? "tick" : "error",
-            );
-        }
-
-        LogStuff(
-            t === "module"
-                ? "This is an ESM project, lovely."
-                : `Using CommonJS is not 'wrong', but not ideal. Consider switching to ESM.${
-                    t ? "" : "\nNote: The 'type' field was not specified, so Node assumes CJS. If you use ESM, specify type='module'."
-                }`,
-            t ? "tick" : "bruh",
-        );
-    } else {
-        // this is very basic tbh, a deno.json doesn't have much
-        const { lint, fmt, lock } = content;
-
-        LogStuff(
-            lint ? `Found lint configurations. That's good!` : "No lint configurations found.",
-            lint ? "tick" : "bruh",
-        );
-        LogStuff(
-            fmt ? `Found formatting configurations. That's good!` : "No formatting configurations found.",
-            fmt ? "tick" : "bruh",
-        );
-        LogStuff(
-            lock ? `Found lockfile configurations. That's good!` : "Why did you disable the lockfile???",
-            lock ? "tick" : "bruh",
-        );
-    }
+    RecommendedCommunityStandards(env);
 }
---
+++ src/commands/toolkit/rcs.ts
@@ -0,0 +1,121 @@
+import { StringUtils } from "@zakahacecosas/string-utils";
+import { ProjectEnvironment } from "../../types/platform.ts";
+import { ColorString, LogStuff } from "../../functions/io.ts";
+import { parse } from "@std/semver";
+
+export function RecommendedCommunityStandards(env: ProjectEnvironment) {
+    // deno-lint-ignore no-explicit-any
+    const content = env.main.stdContent as any;
+
+    const isNodeJs = StringUtils.validateAgainst(env.runtime, ["node", "bun"]);
+    const isDenoJs = env.runtime === "deno";
+
+    if (isNodeJs) {
+        const { version, private: isPrivate, license, author, contributors, description, repository, bugs, type } = content;
+
+        LogStuff(
+            isPrivate ? "This is a private project. Running generic checks." : "This is a public project. Running additional package checks.",
+            "bulb",
+        );
+
+        try {
+            parse(version);
+            LogStuff("Version follows the SemVer format. Nice!", "tick");
+        } catch {
+            LogStuff(`${version} is not a valid version! Follow the SemVer specification.`, "error");
+        }
+        LogStuff(license ? `${license} license, good.` : "No license found. You should specify it!", license ? "tick" : "error");
+        if (!isPrivate || author) {
+            LogStuff(
+                author ? `Made by ${typeof author === "string" ? author : author.name}, nice.` : "No author found. Who made this 'great' code?",
+                author ? "tick" : "error",
+            );
+        }
+        LogStuff(
+            contributors
+                ? `So we've got ${contributors.length} contributor(s), great.`
+                : "No contributors found. If none, that's fine, but if someone helps, mention them.",
+            contributors ? "tick" : "bruh",
+        );
+        LogStuff(
+            description ? `"${ColorString(description, "italic")}", neat description.` : "No description found. What does your code even do?",
+            description ? "tick" : "error",
+        );
+        if (!isPrivate) {
+            LogStuff(repository ? `Known repo URL, awesome.` : "You didn't specify a repository, why?", repository ? "tick" : "error");
+            LogStuff(
+                bugs ? `Known bug-report URL, incredible.` : "You didn't specify where to report bugs. Don't be lazy and accept feedback!",
+                bugs ? "tick" : "error",
+            );
+        }
+
+        LogStuff(
+            type === "module"
+                ? "This is an ESM project, lovely."
+                : `Using CommonJS is not 'wrong', but not ideal. Consider switching to ESM.${
+                    type ? "" : "\nNote: The 'type' field was not specified, so Node assumes CJS. If you use ESM, specify type='module'."
+                }`,
+            type ? "tick" : "bruh",
+        );
+    } else if (isDenoJs) {
+        // this is very basic tbh, a deno.json doesn't have much
+        const { lint, fmt, lock, version } = content;
+
+        try {
+            parse(version);
+            LogStuff("Version follows the SemVer format. Nice!", "tick");
+        } catch {
+            LogStuff(`${version} is not a valid version! Follow the SemVer specification.`, "error");
+        }
+        LogStuff(
+            lint ? `Found lint configurations. That's good!` : "No lint configurations found.",
+            lint ? "tick" : "bruh",
+        );
+        LogStuff(
+            fmt ? `Found formatting configurations. That's good!` : "No formatting configurations found.",
+            fmt ? "tick" : "bruh",
+        );
+        LogStuff(
+            lock ? `Found lockfile configurations. That's good!` : "Why did you disable the lockfile!?",
+            lock ? "tick" : "bruh",
+        );
+    } else {
+        // in this case we know it's cargo
+
+        const { license, authors, description, repository, edition } = content;
+
+        LogStuff(
+            license ? `${license} license, nice.` : "No license found. You should specify one!",
+            license ? "tick" : "error",
+        );
+
+        LogStuff(
+            authors
+                ? `Made by ${authors.join(", ")}. Nice to meet ya!`
+                : "No authors found. You should at least mention yourself, and of course credit any contributor!",
+            authors ? "tick" : "error",
+        );
+
+        LogStuff(
+            description ? `"${ColorString(description, "italic")}", neat description.` : "No description found. What does your project even do?",
+            description ? "tick" : "error",
+        );
+
+        LogStuff(
+            repository
+                ? `A repository was specified, nice.`
+                : "No repository found. Consider adding a repository URL, else how do we contribute bug fixes?",
+            repository ? "tick" : "error",
+        );
+
+        LogStuff(
+            edition ? `Rust ${edition} edition` : "No edition found. You should specify the edition you're using (e.g., 2018, 2021).",
+            edition ? "tick" : "error",
+        );
+
+        // LogStuff(
+        //     features && features.length > 0 ? `Found ${features.length} features.` : "No features found. Consider defining optional features.",
+        //     features && features.length > 0 ? "tick" : "bruh",
+        // );
+    }
+}
