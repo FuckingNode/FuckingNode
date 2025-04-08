17facf6d0089f003691c1f9233b8d0b9d8246006
ZakaHaceCosas <zakahacecosas@protonmail.com>
2025-04-08 21:12:03 +0200

    improve updater's code

--- src/commands/updater.ts
+++ src/commands/updater.ts
@@ -4,59 +4,48 @@ import { LOCAL_PLATFORM, RELEASE_URL, VERSIONING } from "../constants.ts";
 import type { GITHUB_RELEASE } from "../types/misc.ts";
 import { GetDateNow } from "../functions/date.ts";
 import type { TheUpdaterConstructedParams } from "./constructors/command.ts";
-import { ColorString, LogStuff, StringifyYaml } from "../functions/io.ts";
+import { ColorString, Interrogate, LogStuff, StringifyYaml } from "../functions/io.ts";
 import { parse as parseYaml } from "@std/yaml";
 import { GetAppPath } from "../functions/config.ts";
 import type { CF_FKNODE_SCHEDULE } from "../types/config_files.ts";
 import { Commander } from "../functions/cli.ts";
 import { JoinPaths } from "../functions/filesystem.ts";
 
-function TellAboutUpdate(newVer: string): void {
-    LogStuff(
-        `There's a new version! ${newVer}. Consider downloading it from GitHub. You're on ${VERSIONING.APP}, btw.`,
-        "bulb",
-    );
-}
-
 /**
  * Checks for updates.
  *
+ * @async
  * @export
  * @returns {Promise<void>}
  */
 export default async function TheUpdater(params: TheUpdaterConstructedParams): Promise<void> {
     const scheduleFilePath = GetAppPath("SCHEDULE");
-    const scheduleFileContents = parseYaml(await Deno.readTextFile(scheduleFilePath)) as CF_FKNODE_SCHEDULE;
-
-    const IsUpToDate: (tag: string) => boolean = (tag: string) => compare(parse(VERSIONING.APP), parse(tag)) >= 0;
+    const scheduleFileContents = parseYaml(Deno.readTextFileSync(scheduleFilePath)) as CF_FKNODE_SCHEDULE;
 
     async function CheckUpdates(): Promise<CF_FKNODE_SCHEDULE | "rl"> {
-        try {
-            const response = await FetchGitHub(RELEASE_URL);
+        const response = await FetchGitHub(RELEASE_URL);
 
-            if (!response.ok) {
-                if (response.status === 403) return "rl"; // (github has a rate limit, so this is not an error we should be really aware of)
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const content: GITHUB_RELEASE = await response.json();
-
-            const dataToWrite: CF_FKNODE_SCHEDULE = {
-                ...scheduleFileContents,
-                updater: {
-                    lastCheck: GetDateNow(),
-                    latestVer: content.tag_name,
-                },
-            };
-
-            await Deno.writeTextFile(scheduleFilePath, StringifyYaml(dataToWrite));
-            return dataToWrite;
-        } catch (e) {
-            throw new Error("Error checking for updates: " + e);
+        if (!response.ok) {
+            if (response.status === 403) return "rl"; // (github has a rate limit, so this is not an error we should be really aware of)
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
+
+        const content: GITHUB_RELEASE = await response.json();
+
+        const dataToWrite: CF_FKNODE_SCHEDULE = {
+            ...scheduleFileContents,
+            updater: {
+                lastCheck: GetDateNow(),
+                latestVer: content.tag_name,
+            },
+        };
+
+        Deno.writeTextFileSync(scheduleFilePath, StringifyYaml(dataToWrite));
+        return dataToWrite;
     }
 
     const needsToUpdate = await CheckUpdates();
+
     if (needsToUpdate === "rl") {
         LogStuff(
             "Bro was rate-limited by GitHub (update provider). Try again in a few hours.",
@@ -68,24 +57,28 @@ export default async function TheUpdater(params: TheUpdaterConstructedParams): P
 
     const { latestVer } = needsToUpdate.updater;
 
-    if (IsUpToDate(latestVer)) {
+    if (compare(parse(VERSIONING.APP), parse(latestVer)) >= 0) {
         if (params.silent) return;
         LogStuff(`You're up to date! ${ColorString(VERSIONING.APP, "bright-green")} is the latest.`, "tick");
         return;
-    } else {
-        TellAboutUpdate(latestVer);
-        if (!params.install) return;
-        const filename = LOCAL_PLATFORM.SYSTEM === "windows" ? "install.ps1" : "install.sh";
-        const buffer = await (await fetch(
-            LOCAL_PLATFORM.SYSTEM === "windows" ? "https://fuckingnode.github.io/install.ps1" : "https://fuckingnode.github.io/install.sh",
-        )).arrayBuffer();
-
-        const path = Deno.makeTempDirSync({ prefix: "UPDATE-SH" });
-        Deno.writeTextFileSync(JoinPaths(path, filename), new TextDecoder().decode(new Uint8Array(buffer)));
-
-        Commander(
-            LOCAL_PLATFORM.SYSTEM === "windows" ? "iex" : "bash",
-            [JoinPaths(path, filename)],
-        );
     }
+
+    LogStuff(
+        `There's a new version! ${latestVer}. You're on ${VERSIONING.APP}, btw.`,
+        "bulb",
+    );
+    if (!params.install) return;
+    if (!Interrogate("Should we auto-update the CLI for you?")) return;
+    const filename = LOCAL_PLATFORM.SYSTEM === "windows" ? "install.ps1" : "install.sh";
+    const buffer = await (await fetch(
+        `https://fuckingnode.github.io/${filename}`,
+    )).arrayBuffer();
+
+    const path = Deno.makeTempDirSync({ prefix: "UPDATE-SH" });
+    Deno.writeTextFileSync(JoinPaths(path, filename), new TextDecoder().decode(new Uint8Array(buffer)));
+
+    Commander(
+        LOCAL_PLATFORM.SYSTEM === "windows" ? "iex" : "bash",
+        [JoinPaths(path, filename)],
+    );
 }
